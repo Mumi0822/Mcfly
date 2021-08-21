c70185b0fa069a3a717b6125abdb42881e44ce51
Mumi0822 <ykmail1409@gmail.com>
2021-08-21 19:29:53 +0900

    進捗

--- example.py
+++ example.py
@@ -3,33 +3,33 @@ from discord import guild
 from discord.flags import Intents
 
 client = discord.Client()
-
-Grobal_ch_name =""
+Grobal_ch_num = 0
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
 @client.event
 async def on_guild_join(guild):
-    await guild.text_channels[1].send('Thanks your inviting me!!!')
+    await client.guild.text_channels[0].send('Thanks your inviting me!!!')
     return
 
 @client.event
 async def on_message(message):
-    text_ch = Intents.guild.text_channels
+    text_ch = message.guild
     if message.author == client.user:
         return
     if message.content.startswith('G/'):
+        #Grobalchannelを指定
         i = 0
-        while(text_ch[i]):
+        while text_ch.text_channels:
             i+=1
             if message.content.startswitch('G/'+i):
-                print(i)
-                Grobal_ch_name = i
-                print(Grobal_ch_name)
+                await print(i)
+                Grobal_ch_num = i-1
+                await print(Grobal_ch_num)
+                return
+        await message.channel.send('There is no channel!')
     if message.content.startswith('/hello'):
         await message.channel.send('Hello!')
-
-
 
 client.run('')
